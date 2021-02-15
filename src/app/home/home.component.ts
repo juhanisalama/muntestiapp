d6a6b8ac941de42a616f41e42d65b145d04985ab
Juhani Salama <salamajuhani@gmail.com>
2021-02-15 12:01:24 +0200

    testicommit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  clickCounter: number = 0;
+  name: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  countClick() {
+    this.clickCounter += 1;
+  
+  } 
+
+  /*tooMuch() {
+  if (this.clickCounter >= 5)
+  { console.log('Steady on mate!')}
+  }*/
+}
